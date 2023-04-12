90221aff4ed6733b4bb5cd796b9d8d4e26c53875
unknown <sanskruti.pawar8702@gmail.com>
2023-04-12 10:32:57 +0530

    first commit

---
+++ client/src/pages/Plans/Plans.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+
+import PricingCard from "./PricingCard";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import "./Plans.css";
+
+ 
+const Plans =() =>{
+    const cardsData = [
+        {
+          id: 1,
+          type: 'basic',
+          title: 'Silver Plan',
+          description: 'Subscribe here to activate the silver plan',
+          price: 100.00,
+          mostPopular: false,
+          data: ['Having more than 1 doubt per day?', 'Ask 5 questions per day!']
+        },
+        {
+          id: 2,
+          type: 'medium',
+          title: 'Gold Plan',
+          description: 'Subscribe here to activate the gold plan.',
+          price: 1000.00,
+          mostPopular: true,
+          data: [`Still don't feel satisfied?`, 'Ask 10 questions per day!']
+        }
+    ];
+
+    const currentUser = useSelector((state) => state.currentUserReducer);
+    const navigate = useNavigate(); 
+    useEffect(() => {
+        currentUser === null && navigate("/auth")
+      },[currentUser, navigate]);
+    return (
+        <div className="app-wrapper">
+            
+                {/* cardsData.map(props => {                              
+                    return (
+                        <PricingCard
+                            {...props}
+                            key={props.id}
+                        />
+                    );
+                }) */}
+             
+            {cardsData.map((user) => (
+                <PricingCard
+                            {...user}
+                            key={user.id}
+                        />
+      ))}
+        </div> 
+);
+}
+export default Plans; 
---
+++ client/src/pages/Plans/PricingCard.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import "./Plans.css";
+
+function CardDescription({ title, description }) {	
+	return (
+		<div className="card-description">
+			<h2>{ title }</h2>
+			<p>{ description }</p>
+		</div>
+	);
+};
+
+function CardBilling({ price }) {
+	return (
+		<div className="card-billing">
+			<p>
+				<strong className="price">₹ { price }</strong>
+        <strong> / mo.</strong>
+			</p>
+			<p>
+				<span className="recurrency">
+					Billed ₹{price}/monthly
+				</span>
+			</p>
+		</div>
+	);
+};
+
+function CardFeatures({ data }) {	
+	return (
+		<div className="card-features">
+			<ul>
+				{ 
+					data.map((item, index) => {
+						return (
+							<li key={index}>{item}</li>
+						)
+					})
+				}
+			</ul>
+		</div>
+	);
+};
+
+function PricingCard(props) {	
+    const { 
+      type,
+      title,
+      description,
+      price,
+      mostPopular,
+      data,
+    } = props;
+    console.log(type)
+    return (
+		<div className="cardbox">
+			<div className={`card pricing-card ${type}`}>
+        { (mostPopular) ? <span className="most-popular">Most Popular</span> : null }
+			<CardDescription title={title} description={description} />
+			<CardBilling price={price} />
+	<CardFeatures data={data} />
+	{/*<CardAction type={type} description={description} price={price} />*/}
+	  </div>
+		</div>
+      
+    );
+};
+
+export default PricingCard;
